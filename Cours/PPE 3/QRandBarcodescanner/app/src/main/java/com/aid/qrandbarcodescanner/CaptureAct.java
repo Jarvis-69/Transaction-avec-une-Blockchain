9653e5ea987ec6361830803bb64ecfa866ec9933
Jarvis-69 <94354305+Jarvis-69@users.noreply.github.com>
2022-05-22 19:01:43 +0200

    Delete Cours directory

--- Cours/PPE 3/QRandBarcodescanner/app/src/main/java/com/aid/qrandbarcodescanner/CaptureAct.java
+++
@@ -1,9 +0,0 @@
-package com.aid.qrandbarcodescanner;
-
-import com.journeyapps.barcodescanner.CaptureActivity;
-
-public class CaptureAct extends CaptureActivity {
-
-
-
-}
--- Cours/SLAM/PHP/ExercicesPart2/tp01/config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-try {
-$pdo = new PDO("mysql:host=localhost;port=3309;dbname=tp", "root");
-}
-catch(PDOException $e) {
-    echo $e->getMessage();
-}
-
